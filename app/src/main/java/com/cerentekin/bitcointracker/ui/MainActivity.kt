eb79a0e8946cb2dc8103558ccb546b1b3fe2cf74
Ceren Tekin <cerentekin03@outlook.com>
2025-06-11 20:25:56 +0300

    Initial commit

---
+++ app/src/main/java/com/cerentekin/bitcointracker/ui/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.cerentekin.bitcointracker.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.cerentekin.bitcointracker.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
